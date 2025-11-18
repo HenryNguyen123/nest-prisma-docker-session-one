e0b27bc46d2ca9f400aa30c627f9c927f3028022
Minh Nhat <nhokkudo143@gmail.com>
2025-11-18 21:29:32 +0700

    add cors

---
+++ src/config/configCors.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+export const cors = (app: any) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: 'GET,POST,OPTIONS,PUT,PATCH,DELETE',
+  });
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
-
+import { cors } from './config/configCors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  cors(app);
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
