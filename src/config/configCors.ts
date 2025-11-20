f1b234d02fee90a022a1c879f3d139a12d36c1ee
Minh Nhat <nhokkudo143@gmail.com>
2025-11-20 15:41:04 +0700

    check cors

--- src/config/configCors.ts
+++ src/config/configCors.ts
@@ -3,7 +3,8 @@ import 'dotenv/config';
 export const cors = (app: any) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
+    origin: ['http://localhost:3000', 'https://henrynguyen123.github.io'],
     credentials: true,
     methods: 'GET,POST,OPTIONS,PUT,PATCH,DELETE',
   });
