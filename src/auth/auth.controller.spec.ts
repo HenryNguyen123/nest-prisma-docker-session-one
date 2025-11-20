31e52108aa3c296c1306647dba0ab1a3ea028ac9
Minh Nhat <nhokkudo143@gmail.com>
2025-11-21 00:11:39 +0700

    fix test e2e

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +10,11 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        JwtService,
+        { provide: PrismaService, useValue: {} }, // mock PrismaService
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
