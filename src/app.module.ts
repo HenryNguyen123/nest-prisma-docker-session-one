35768fd0614aec5a999c419c9ab98bbd661ed382
Minh Nhat <nhokkudo143@gmail.com>
2025-11-25 22:01:12 +0700

    create module prisma and config add in app module and create folder types

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,13 @@ import { AuthModule } from './auth/auth.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { MailerModule } from '@nestjs-modules/mailer';
 // import { existsSync, mkdirSync } from 'fs';
+import { MailService } from './mail/mail.service';
+import { MailController } from './mail/mail.controller';
+import { MailModule } from './mail/mail.module';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { PrismaModule } from 'src/prisma.module';
 
 // const uploadPath = join(__dirname, '..', 'public', 'images', 'avatar');
 // if (!existsSync(uploadPath)) {
@@ -14,15 +20,41 @@ import { join } from 'path';
 @Module({
   imports: [
     AuthModule,
+    // step1: base public
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), '..', 'public'),
       serveRoot: '/public',
     }),
+    // step2: client get path images
     MulterModule.register({
       dest: join(process.cwd(), '..', 'public', 'images', 'avatar'),
     }),
+    //step2: setup mailer
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com', // host SMTP
+        port: 465, // port SSL
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER, // email gửi
+          pass: process.env.EMAIL_PASS, // app password nếu dùng Gmail
+        },
+      },
+      defaults: {
+        from: '"No Reply" <no-reply@example.com>',
+      },
+      template: {
+        dir: join(__dirname, '..', 'templates'), // folder chứa file email template
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+    MailModule,
+    PrismaModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MailController],
+  providers: [AppService, MailService],
 })
 export class AppModule {}
