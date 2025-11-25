6e4973b957e873c1e86c7ae535745a7b2dea4809
Minh Nhat <nhokkudo143@gmail.com>
2025-11-25 21:59:11 +0700

    create utils with jwt

---
+++ src/utils/jwt/jwt.utils.ts
@@ -0,0 +1,36 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
+
+interface SignJwtType {
+  payload: object;
+  secret: string;
+  expiresIn: JwtSignOptions['expiresIn'];
+}
+const jwt = new JwtService();
+export const signJWT = async (data: SignJwtType) => {
+  try {
+    const token = await jwt.signAsync(data.payload, {
+      secret: data.secret,
+      expiresIn: data.expiresIn ?? 900,
+    });
+    return token;
+  } catch (error: unknown) {
+    console.log(error);
+    throw new HttpException(
+      { message: 'verify password error' },
+      HttpStatus.UNAUTHORIZED,
+    );
+  }
+};
+
+export const verifyJWT = async (token: string, key: string) => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return await jwt.verifyAsync(token, {
+      secret: key,
+    });
+  } catch (error: unknown) {
+    console.log(error);
+    return false;
+  }
+};
