73952df1d1be34c2315e3ea5ab6d52ccf6f1a52e
Minh Nhat <nhokkudo143@gmail.com>
2025-11-26 12:36:20 +0700

    fix build templates 123

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -38,7 +38,13 @@ export class MailController {
           HttpStatus.UNAUTHORIZED,
         );
       }
-      return responseError('Internal server error', -500);
+      const errorString = JSON.stringify(
+        error,
+        Object.getOwnPropertyNames(error),
+        2,
+      );
+
+      return responseError(errorString, -500);
     }
   }
   //step: forgot password user
