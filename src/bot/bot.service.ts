5597488d345d4156663c820b46862c3661187a0b
Minh Nhat <nhokkudo143@gmail.com>
2025-12-07 22:52:02 +0700

    fix bot

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,11 +1,22 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class BotService {
-  private readonly logger = new Logger(BotService.name);
+export class PingBotService {
+  private readonly logger = new Logger(PingBotService.name);
+  private readonly url = `${process.env.DOMAIN_SERVER}/ping`;
+
+  constructor(private readonly httpService: HttpService) {}
+
   @Cron(CronExpression.EVERY_5_MINUTES)
-  handleTask() {
-    this.logger.log('Bot is alive and working!');
+  async pingRender() {
+    try {
+      const response = await firstValueFrom(this.httpService.get(this.url));
+      this.logger.log(`Ping successful: ${JSON.stringify(response.data)}`);
+    } catch (error) {
+      this.logger.error(`Ping failed: ${error}`);
+    }
   }
 }
