c9aaf6596fae4517c2a8fcd067d401b073c709be
Minh Nhat <nhokkudo143@gmail.com>
2025-12-07 23:05:49 +0700

    fix bot 1s

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot.service';
 import { BotController } from './bot.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [BotService],
   controllers: [BotController],
 })
